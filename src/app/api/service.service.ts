2943136f5d81514e9bd9b2bc579d9c7769029efb
leonardoCatharin <leo_catharin@hotmail.com>
2022-04-25 22:17:55 -0300

    implementada a service generica

--- src/app/api/service.service.ts
+++ src/app/api/service.service.ts
@@ -7,5 +7,50 @@ import {Md5} from 'ts-md5/dist/md5';
   providedIn: 'root'
 })
 export class ServiceService {
+  private publicKey  = "";
+  private privateKey = "";
+
+  private host = "http://gateway.marvel.com/";
+
+  constructor(private http: HttpClient){}
+
+  public getDados(url: string, parameters: string){
+    let ts = this.generateTs();
+    
+    return new Promise((ret) => {
+      this.http.get(this.host + url + '?ts=' 
+      + ts + '&apikey=' + this.publicKey 
+      + '&hash=' + this.getHash(ts) + parameters).subscribe((response) => {
+        
+        if(response){
+          ret(response);
+
+        } else {
+          ret(false);
+        }
+      })          
+    })
+
+  }
   
+  private generateTs(){
+    return Math.floor(100000 + Math.random() * 900000);
+  }
+
+  private getHash(ts){
+    return Md5.hashStr(ts + this.privateKey + this.publicKey);
+  }
+
+  private getKeys(){
+    return new Promise((ret) => {
+      this.http.get('assets/keys.json').subscribe((keys:any) => {
+        this.publicKey  = keys.public;
+        this.privateKey = keys.private;
+
+        ret(true);
+      })
+    })
+  }
+
+
 }
