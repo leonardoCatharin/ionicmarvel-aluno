4f634f4556242a8b3a4a4f713d92034743c55237
leonardoCatharin <leo_catharin@hotmail.com>
2022-05-02 22:14:40 -0300

    implementados metodos do provider character.service

--- src/app/provider/character.service.ts
+++ src/app/provider/character.service.ts
@@ -7,7 +7,60 @@ import { PaginationComponent } from './../util/pagination/pagination.component';
 })
 export class CharacterService {
 
-  constructor(private service: ServiceService) { }
+  constructor(private service: ServiceService) {}
+
+  public getCharacterById(id: number){
+    return new Promise((ret) => {
+      this.service.getDados('/v1/public/characters/' + id, '')
+      .then((data: any) => {
+          if(data && data.data && data.data.results){
+            ret(data.data.results);
+
+          } else {
+
+            ret([]);
+          }
+
+      })
+    })
+  }
+
+  public getAllCharacters(pagination: PaginationComponent, filter: string){
+    let strFilter = '';
+
+    let param = '&limit=' 
+    + pagination.getLimit() + 
+    '&offset=' + pagination.getOffset() + strFilter;
+
+    return new Promise((ret) => {
+      this.service.getDados('/v1/public/characters', 
+      param).then((data:any) => {
+        if(data && data.data && data.data.results){
+          this.updatePagination(pagination, data.data);
+          ret(data.data.results);
+
+        } else {
+          ret([]);
+
+        }
+      })
+    })
+
+  }
+
+  public getComicsByCharacter(id: number){
+    return new Promise((ret) => {
+      this.service.getDados('/v1/public/characters/' + id + '/comics', '').then((data:any) => {
+        if(data && data.data && data.data.results){
+          ret(data.data.results);
+
+        } else {
+          ret([]);
+          
+        }
+      })
+    })
+  }
 
   /* atualiza as informações de paginação */
   private updatePagination(pagination: PaginationComponent, data: any){
